9f0a44e16022f1f6fa8e6e795582c260758eb120
Kyle Whittemore <kyle.whittemore@gmail.com>
2020-01-04 00:17:48 -0500

    added comment header

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,11 @@
+// =====================================================
+//    The Eslint configuration below can be adjusted to 
+//  suit your needs.  Please visit the ESLint website at
+//  https://eslint.org/docs/user-guide/configuring for 
+//  information on how to do so.  The configuration is 
+//  set to use the Airbnb style recommendations.
+// =====================================================
+
 module.exports = {
   env: {
     browser: true,
