78fcca3cb88ece65a620fce0b34be1663b2756aa
Kyle Whittemore <kyle.whittemore@gmail.com>
2020-01-05 09:17:13 -0500

    added jawsDB environmental variable for heroku deploy

--- config/config.js
+++ config/config.js
@@ -16,6 +16,7 @@ module.exports = {
     dialect: 'mysql',
   },
   production: {
+    use_env_variable: 'JAWSDB_URL',
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
